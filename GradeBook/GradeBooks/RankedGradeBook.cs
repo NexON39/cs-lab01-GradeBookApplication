364ddf3779cadc069078395a80ab4ec79c2830c4
NexON39 <flashxd30501337@gmail.com>
2023-03-23 08:46:46 +0100

    override getLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks;
@@ -9,5 +11,26 @@ public class RankedGradeBook : BaseGradeBook
     ) : base(name)
     {
         Type = GradeBookType.Ranked;
+    }
+
+    public override char GetLetterGrade(double averageGrade)
+    {
+        if(Students.Count < 5)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var separatedCountStudents = (int)Math.Ceiling(Students.Count * 0.2);
+        var studentGrades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+        if (averageGrade >= studentGrades[separatedCountStudents - 1])
+            return 'A';
+        if (averageGrade >= studentGrades[(separatedCountStudents * 2) - 1])
+            return 'B';
+        if (averageGrade >= studentGrades[(separatedCountStudents * 3) - 1])
+            return 'C';
+        if (averageGrade >= studentGrades[(separatedCountStudents * 4) - 1])
+            return 'D';
+        return 'F';
     }
 }
